eba787d113ee31f0a0a6297a322b8c442989cec7
Patolll <patryk_dmochowski@wp.pl>
2025-10-18 23:35:56 +0200

    header and hero

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+const navLinks = document.querySelector(".nav-links");
+
+document.getElementById("menu-toggle").addEventListener("click", function (e) {
+  e.target.name =
+    e.target.name === "menu-outline" ? "close-outline" : "menu-outline";
+  navLinks.classList.toggle("top-[13%]");
+});
+
+const images = ["masaz.jpeg", "masaz2.jpg", "masaz3.jpg"];
+let current = 0;
+const hero = document.getElementById("hero");
+
+setInterval(() => {
+  current = (current + 1) % images.length;
+  hero.style.backgroundImage = `url('${images[current]}')`;
+}, 5000);
